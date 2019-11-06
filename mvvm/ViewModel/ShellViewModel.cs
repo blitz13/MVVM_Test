316d1de4673671cfacfcd4ed7dabb67a28733f9b
Eizel del Monte <eizel.del-monte@ubisoft.com>
2019-11-06 10:47:46 +0800

    Added vs2017(C#)  files from local to remote

---
+++ mvvm/ViewModel/ShellViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvm.ViewModel
+{
+    public class ShellViewModel : Screen
+    {
+        //This is a test
+    }
+}
